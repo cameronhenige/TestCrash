95f09b9ab756769746f3b3bfba98a25e93fbb713
Cameron Henige <godmodecameron@gmail.com>
2021-07-16 22:26:34 -0400

    Initial Commit

---
+++ TestCrash/TestCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCrashApp.swift
+//  TestCrash
+//
+//  Created by Hannah Krolewski on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TestCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
